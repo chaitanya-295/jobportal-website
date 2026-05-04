dcc83b6ecf87100dc0ac93bd0ccd97cafffbf091
Chaitanya Kamble <chaitanyakamble2005@gmail.com>
2026-05-04 19:26:11 +0530

    Fix: Added manual image serving for production

--- jobportal/jobportal/urls.py
+++ jobportal/jobportal/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from job.views import *
 from job import views
-from django.conf import settings, settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -62,4 +63,12 @@ urlpatterns = [
     path('edit_user_profile/', views.edit_user_profile, name='edit_user_profile'),
     path('change_password_user/', views.change_password_user, name='change_password_user'),
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
